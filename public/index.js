d450707178f0cbc96dba574f770c873fd1c6a9d2
Oladapo01 <91552016+Oladapo01@users.noreply.github.com>
2023-03-22 21:21:40 +0000

    Partially working code

--- public/index.js
+++ public/index.js
@@ -1,6 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
 function SearchForm() {
     const [region, setRegion] = React.useState("");
     const [pointOfInterest, setPointOfInterest] = React.useState([]);
+
+    React.useEffect(() => {
+        const map = L.map('map').setView([0, 0], 2);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data © OpenStreetMap contributors',
+        }).addTo(map);
+
+        //Add a marker layer for the point of interest
+        const poiMarker = L.markerClusterGroup();
+        poiMarker.addTo(map);
+
+        //Add a click listener for the point of interest
+        poiMarker.on('click', (event) => {
+            const poi = event.layer.poi;
+            L.popup()
+                .setLatLng(event.latlng)
+                .setContent(`<h2>${poi.name}</h2><p>${poi.description}</p>`)
+                .openOn(map);
+        });
+
+        //Update the marker layer whenever the pointOfInterest state changes
+        poiMarker.clearLayers();
+        for (const poi of pointOfInterest) {
+            L.marker([poi.lat, poi.lon], {poi})
+                .addTo(poiMarker)
+                .bindPopup(`<h2>${poi.name}</h2><p>${poi.description}</p>`);
+            }
+        }, [pointOfInterest]);
 
     const handleRegionChange = (event) => {
         setRegion(event.target.value);
@@ -42,20 +75,7 @@ function SearchForm() {
                 </label>
                 <input type="submit" value="Search" />
             </form>
-            {pointOfInterest.length > 0 && 
-            <ul>
-                {pointOfInterest.map((poi) => (
-                    <li key={poi.id}>
-                        <h2>{poi.name}</h2>
-                        <h4><p>Description: {poi.description}</p></h4>
-                        <p>Latitude: {poi.lat}</p>
-                        <p>Longitude: {poi.lon}</p>
-                        <button onClick={() => handleRecommend(poi.id)}>Recommend</button>
-                    </li>
-                )
-                )}   
-            </ul>
-            }
+            <div id="map" style={{ height: "500px" }}></div>
         </div>
         );
 
