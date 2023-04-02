9de9c31c4ad0dd47d9dbe1fd289125228b946e44
Oladapo01 <91552016+Oladapo01@users.noreply.github.com>
2023-04-02 18:48:31 +0100

    The popup form works but I need to get the marker to appear

--- public/index.js
+++ public/index.js
@@ -4,6 +4,7 @@ function SearchForm() {
     const [region, setRegion] = React.useState("");
     const [pointOfInterest, setPointOfInterest] = React.useState([]);
     const mapContainerRef = React.useRef(null);
+    const mapRef = React.useRef(null);
     const [addPOI, setAddPOI] = React.useState(false);
     const [newPOI, setNewPOI] = React.useState({
         name: "",
@@ -16,6 +17,8 @@ function SearchForm() {
         recommendations: 0
     });
     const [addPOIMessage, setAddPOIMessage] =React.useState("");
+    let lat = 0;
+    let lon = 0;
 
     const handleRegionChange = (event) => {
         setRegion(event.target.value);
@@ -51,43 +54,45 @@ function SearchForm() {
         }
     };
 
-    const handleMapClick = (event) => {
-        if(event.latlng){
-            setNewPOI({
-                ...newPOI,
-                lat: event.latlng.lat,
-                lon: event.latlng.lng,
-            });
-            setAddPOI(true);
-        }
-
-        if (mapContainerRef.current && event.latlng) {
-            L.marker([event.latlng.lat, event.latlng.lng]).addTo(mapContainerRef.current);
-        }
-       
-    };
-
-    const handleAddPOI = async () => {
+    
+    const handleAddPOI = async (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const type = form.type.value;
+        const country = form.country.value;
+        const region = form.region.value;
+        const description = form.description.value;
         try {
             const response = await fetch(`/poi/add`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify(newPOI),
+                body: JSON.stringify({
+                    name: name,
+                    type: type,
+                    country: country,
+                    region: region,
+                    lon: newPOI.lon,
+                    lat: newPOI.lat,
+                    description: description,
+                    recommendations: 0  
+                }),
             });
-            if(response.ok) {
-                setAddPOIMessage("Point of interest added");
-                setAddPOI(false);
+            if(response.ok) { 
                 const data = await response.json();
                 const poiId = data.id;
+                setAddPOIMessage("Point of interest added");
+                setAddPOI(false);
+
                 // Reload the point of interest list
                 const response2 = await fetch(`/regions/${region}`);
                 const data2 = await response2.json();
                 setPointOfInterest(data2);
                 // Add the new marker to the map
                 const marker = L.marker([newPOI.lat, newPOI.lon]).addTo(mapContainerRef.current);
-                marker.bindPopup(`<h2>${newPOI.name}</h2><p>${newPOI.description}</p>`);
+                marker.bindPopup(`<h2>${newPOI.name}</h2><p>${newPOI.description}</p>`).openPopup();
             }else {
                 const data = await response.json();
                 setAddPOIMessage(data.error);
@@ -96,12 +101,49 @@ function SearchForm() {
             console.log(error);
             setAddPOIMessage("Error adding point of interest. Please try again");  
         }
+        mapContainerRef.current.closePopup();
     };
+    const handleMapClick = (event) => {
+            setAddPOI(true);
+            const {lat, lng} = event.latlng;
+            setNewPOI((prevState) => ({
+               ...prevState,
+                lat: lat,
+                lon: lng,
+            }));
+
+            const domDiv = document.createElement("div");
+            domDiv.innerHTML = `<form id= "form">
+            <label>Name:</label>
+            <input type="text" placeholder="Enter name" id="name" required/><br>
+            <label>Type:</label>
+            <input type="text" placeholder="Enter type" id="type" required/><br>
+            <label>Country:</label>
+            <input type="text" placeholder="Enter country" id="country" required/><br>
+            <label>Region:</label>
+            <input type="text" placeholder="Enter region" id="region" required/><br>
+            <label>Description:</label>
+            <input type="text" placeholder="Enter description" id="description" required/><br>
+            <button type="submit" id="add">Add</button>
+            </form>`;
+            const popup = L.popup()
+                .setLatLng([lat, lng])
+                .setContent(domDiv)
+                .openOn(mapRef.current);
+            const form = domDiv.querySelector("form");
+            // Add the event listener for the form submission
+            form.addEventListener("submit", (event) => {
+                handleAddPOI(event, lat, lng)
+            }); 
+            
+        };
     
     React.useEffect(() => {
         let map = null;
         if (mapContainerRef.current){
             map = L.map(mapContainerRef.current).setView([50.908, -1.4], 14);
+            // Set the map instance to the mapRef
+            mapRef.current = map;
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
@@ -112,15 +154,21 @@ function SearchForm() {
                 marker.bindPopup(`<h2>${poi.name}</h2><p>${poi.description}</p>`);
                 markers.push(marker);
             });
+            // Add the event listener for the map click
+            map.on("click", handleMapClick);
+
             // Clean up the markers when the component unmounts
             return () => {
                 if(map){
                     map.remove();
                 }
-                markers.forEach((marker) => marker.remove()); 
-            };
-        }
-        
+                markers.forEach((marker) => marker.remove());
+                if(map)
+                {
+                    map.off("click", handleMapClick)
+                } 
+            }    
+        } 
     }, [pointOfInterest]);
 
     return(
@@ -132,44 +180,14 @@ function SearchForm() {
                 </label>
                 <input type="submit" value="Search" />
             </form>
-            <div ref={mapContainerRef} style={{height: "400px"}} onClick={handleMapClick}></div>
+            <div ref={mapContainerRef} style={{height: "400px"}} /*onClick={handleMapClick}*/></div>
             {addPOI &&
-                <div>
-                    <h2>Add a point of interest</h2>
-                    <form>
-                        <label>
-                            Name:
-                            <input type="text" value={newPOI.name} onChange={(event) => setNewPOI({...newPOI, name: event.target.value})} />
-                        </label>
-                        <label>
-                            Type:
-                            <input type="text" value={newPOI.type} onChange={(event) => setNewPOI({...newPOI, type: event.target.value})} />
-                        </label>
-                        <label>
-                            Country:
-                            <input type="text" value={newPOI.country} onChange={(event) => setNewPOI({...newPOI, country: event.target.value})} />
-                        </label>
-                        <label>
-                            Region:
-                            <input type="text" value={newPOI.region} onChange={(event) => setNewPOI({...newPOI, region: event.target.value})} />
-                        </label>
-                        <label>
-                            Latitude:
-                            <input type="text" value={newPOI.lat} onChange={(event) => setNewPOI({...newPOI, lat: event.target.value})} />
-                        </label>
-                        <label>
-                            Longitude:
-                            <input type="text" value={newPOI.lon} onChange={(event) => setNewPOI({...newPOI, lon: event.target.value})} />
-                        </label>
-                        <label>
-                            Description:
-                            <input type="text" value={newPOI.description} onChange={(event) => setNewPOI({...newPOI, description: event.target.value})} />
-                        </label>
-                        <input type="button" value="Add" onClick={handleAddPOI} />
-                    </form>
-                    <p>{addPOIMessage}</p>
-                </div>
+            <div>
+                <h2>Add point of interest</h2>
+                {addPOIMessage !== "" && <p>{addPOIMessage}</p>}
+            </div>
             }
+            
             {pointOfInterest.length > 0 && 
             <ul>
                 {pointOfInterest.map((poi) => (
